b071bb5a0c14e2c502bb4718e46f018d810ddcee
Jolypunk <114174400+Jolypunk@users.noreply.github.com>
2022-11-14 01:17:50 +0300

    Add files via upload

---
+++ пр9.1.py
@@ -0,0 +1,27 @@
+a=[]
+k=0
+s=0
+with open('E:\python\практическая 9\\пр9.1.txt','r') as f:
+    for line in f.readlines():
+        b=line.replace('\n','').split()
+        for i in range(len(b)):
+            b[i]=int(b[i])
+        a.append(b)
+    print(a)
+    print('Исходный:')
+for i in range(len(a)):
+    for j in range(len(a[i])):
+        print(a[i][j],' ',end='')
+    print()
+for i in range(len(a)):
+    for j in range(i+1, len(a)):
+        if a[i][j] <= 0:
+            continue
+        if a[i][j] > 0:
+            s += a[i][j]
+            k+=1
+print('Сумма:', s)
+print('Число:', k)
+with open('E:\python\практическая 9\\пр9.2.txt','w') as t:
+    t.write('Summa:'+ " " + str(s) + '\n')
+    t.write('Chislo:'+ " " + str(k) + '\n')
